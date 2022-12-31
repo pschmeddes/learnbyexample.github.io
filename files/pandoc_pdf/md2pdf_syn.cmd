a6c5b89b389d8418e8ed35b0a0041b7d5d69c342
Paul <48135185+pschmeddes@users.noreply.github.com>
2022-12-31 15:35:38 +0100

    Create md2pdf_syn.cmd

---
+++ files/pandoc_pdf/md2pdf_syn.cmd
@@ -0,0 +1,34 @@
+::===============================================================
+:: This scripts converts a .md (markdown) file to a .pdf
+::
+::===============================================================
+echo off
+:: Command line parameter check 
+REM IF NOT ERRORLEVEL 1 GOTO Syntax
+IF "%~1"=="" GOTO Syntax
+IF "%~2"=="" GOTO Syntax
+:: Check sourefile exists
+set sourcefile=%~1
+if not exist "%sourcefile%" (
+    echo Cannot find source file %1
+    goto:EOF
+)
+:: Pandoc conversion
+echo converting %~1 to %~2 please wait...
+pandoc "%~1" ^
+    -f gfm ^
+    --include-in-header chapter_break.tex ^
+    --include-in-header inline_code.tex ^
+    --highlight-style pygments.theme ^
+    -V linkcolor:blue ^
+    -V geometry:a4paper ^
+    -V geometry:margin=2cm ^
+    -V mainfont="DejaVu Serif" ^
+    -V monofont="DejaVu Sans Mono" ^
+    --pdf-engine=xelatex ^
+    -o "%~2"
+echo conversion completed
+goto:EOF
+:Syntax 
+ECHO Convert markdown to pdf 
+ECHO Usage: md2pdf [ markdownsourcefile ] [ pdfFileName]
